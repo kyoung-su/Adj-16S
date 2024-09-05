0a71732d2306653e47422e6673cda70a5a7e48c6
kyoung-su <57653096+kyoung-su@users.noreply.github.com>
2024-09-05 17:18:06 +0900

    Update 01.fastp.py

--- python_codes/01.fastp/01.fastp.py
+++ python_codes/01.fastp/01.fastp.py
@@ -1,1 +1,34 @@
+#!/usr/bin/phtyon
+# -*- coding: UTF-8 -*-
 
+import os
+import os.path
+import time
+
+class SYSTEM_CMD:
+
+    def __init__(self):
+
+        os.chdir(os.getcwd())
+
+        DIR_FILE = os.getcwd()
+
+        MAX_NUM = 9000
+        
+        for X in range(5000, MAX_NUM):
+
+         
+        	FileName01 = f"SRR1300{X}_1.fastq"
+        	FileName02 = f"SRR1300{X}_2.fastq"
+        	FileName03 = f"SRR1300{X}_1.fastp.fastq"
+        	FileName04 = f"SRR1300{X}_2.fastp.fastq"
+        	
+        	if os.path.isfile(f"{DIR_FILE}{FileName01}") and os.path.isfile(f"{DIR_FILE}{FileName02}"):
+        	    CMD = f"fastp -i {FileName01} -I {FileName02} -o {FileName03} -O {FileName04}"
+        	    os.system(CMD)
+        	    time.sleep(1)
+        	    
+        	else:
+        	    pass
+
+SYSTEM_CMD()
