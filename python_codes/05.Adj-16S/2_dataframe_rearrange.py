2668fb5e3de989d63909208ea9f826cdfd7cfb48
kyoung-su <57653096+kyoung-su@users.noreply.github.com>
2024-12-02 18:02:16 +0900

    Update 2_dataframe_rearrange.py

--- python_codes/05.Adj-16S/2_dataframe_rearrange.py
+++ python_codes/05.Adj-16S/2_dataframe_rearrange.py
@@ -4,10 +4,10 @@ import pandas as pd
 taxonomy_df = pd.read_csv('taxonomy_fil_filtered.tsv', sep='\t')
 feature_table_df = pd.read_csv('feature-table.tsv', sep='\t')
 
-# Replace the second column in feature_table_df with the Taxon_fil_filtered column from taxonomy_df
-feature_table_df.iloc[:, 1] = taxonomy_df['Taxon_fil_filtered']
+# Insert the Taxon_fil_filtered column as the second column in feature_table_df
+feature_table_df.insert(1, 'Taxon_fil_filtered', taxonomy_df['Taxon_fil_filtered'])
 
 # Save the updated feature table back to a file
 feature_table_df.to_csv('feature-table_updated.tsv', sep='\t', index=False)
 
-print("The second column in 'feature-table.tsv' has been updated.")
+print("The 'Taxon_fil_filtered' column has been added as the second column in 'feature-table_updated.tsv'.")
