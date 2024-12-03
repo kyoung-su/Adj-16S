93ac7a2f25210981c00610b95d5037d31ed51f7e
kyoung-su <57653096+kyoung-su@users.noreply.github.com>
2024-12-03 15:57:18 +0900

    Update 01.fastp.py

--- python_codes/01.fastp/01.fastp.py
+++ python_codes/01.fastp/01.fastp.py
@@ -1,34 +1,29 @@
-#!/usr/bin/phtyon
+#!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
 import os
-import os.path
 import time
+import glob
 
 class SYSTEM_CMD:
-
     def __init__(self):
-
         os.chdir(os.getcwd())
-
         DIR_FILE = os.getcwd()
 
-        MAX_NUM = 9000
-        
-        for X in range(5000, MAX_NUM):
+        # Find all files ending with _1.fastq and _2.fastq
+        file_pairs = {}
+        for file in glob.glob("*_1.fastq"):
+            base_name = file.replace("_1.fastq", "")
+            paired_file = f"{base_name}_2.fastq"
+            if os.path.isfile(paired_file):
+                file_pairs[base_name] = (file, paired_file)
 
-         
-        	FileName01 = f"SRR1300{X}_1.fastq"
-        	FileName02 = f"SRR1300{X}_2.fastq"
-        	FileName03 = f"SRR1300{X}_1.fastp.fastq"
-        	FileName04 = f"SRR1300{X}_2.fastp.fastq"
-        	
-        	if os.path.isfile(f"{DIR_FILE}{FileName01}") and os.path.isfile(f"{DIR_FILE}{FileName02}"):
-        	    CMD = f"fastp -i {FileName01} -I {FileName02} -o {FileName03} -O {FileName04}"
-        	    os.system(CMD)
-        	    time.sleep(1)
-        	    
-        	else:
-        	    pass
+        for base_name, (FileName01, FileName02) in file_pairs.items():
+            FileName03 = f"{base_name}_1.fastp.fastq"
+            FileName04 = f"{base_name}_2.fastp.fastq"
+            
+            CMD = f"fastp -i {FileName01} -I {FileName02} -o {FileName03} -O {FileName04}"
+            os.system(CMD)
+            time.sleep(1)
 
 SYSTEM_CMD()
