26d6c57c632f12de2a4a23f815d366cdf1ae7cb7
kyoung-su <57653096+kyoung-su@users.noreply.github.com>
2024-12-03 15:55:54 +0900

    Update 02.trim_fastp.py

--- python_codes/02.trim_fastp/02.trim_fastp.py
+++ python_codes/02.trim_fastp/02.trim_fastp.py
@@ -1,34 +1,30 @@
-#!/usr/bin/phtyon
+#!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
 import os
-import os.path
 import time
+import glob
 
 class SYSTEM_CMD:
-
     def __init__(self):
-
         os.chdir(os.getcwd())
-
         DIR_FILE = os.getcwd()
 
-        MAX_NUM = 9000
-        
-        for X in range(5000, MAX_NUM):
+        # Find all files ending with _1.fastp.fastq and _2.fastp.fastq
+        file_pairs = {}
+        for file in glob.glob("*_1.fastp.fastq"):
+            base_name = file.replace("_1.fastp.fastq", "")
+            paired_file = f"{base_name}_2.fastp.fastq"
+            if os.path.isfile(paired_file):
+                file_pairs[base_name] = (file, paired_file)
 
-         
-        	FileName01 = f"SRR1300{X}_1_fastp.fastq"
-        	FileName02 = f"SRR1300{X}_2_fastp.fastq"
-        	FileName03 = f"SRR1300{X}_1_trim.fastp.fastq"
-        	FileName04 = f"SRR1300{X}_2_trim.fastp.fastq"
-        	
-        	if os.path.isfile(f"{DIR_FILE}{FileName01}") and os.path.isfile(f"{DIR_FILE}{FileName02}"):
-        	    CMD = f"fastp -i {FileName01} -I {FileName02} -b 300 -B 208 -o {FileName03} -O {FileName04}"
-        	    os.system(CMD)
-        	    time.sleep(1)
-        	    
-        	else:
-        	    pass
+        for base_name, (FileName01, FileName02) in file_pairs.items():
+            FileName03 = f"{base_name}_1_trim.fastp.fastq"
+            FileName04 = f"{base_name}_2_trim.fastp.fastq"
+            
+            CMD = f"fastp -i {FileName01} -I {FileName02} -b 300 -B 208 -o {FileName03} -O {FileName04}"
+            os.system(CMD)
+            time.sleep(1)
 
 SYSTEM_CMD()
+#If the trimming position changes, the user only needs to modify the number.
